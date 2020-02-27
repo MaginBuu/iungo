92ba63559763cdcbd7eecede7ff6a0846edb6403
Plumoll <guillemfriweb@gmail.com>
2020-02-27 12:23:04 +0100

    inici usuari

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -3,13 +3,10 @@ package com.model;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -18,20 +15,64 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 2681531852204068105L;
 	@Id @GeneratedValue(generator="system-uuid")
 	@GenericGenerator(name="system-uuid", strategy = "uuid")
+	@Column(name = "USER_ID")
 	private String userId;
+
+	@Column(name = "NAME")
+	@NotNull
+	private String name;
+
+	@Column(name = "SURNAME")
+	@NotNull
+	private String surname;
+
+	@Column(name = "SECOND_SURNAME")
+	private String secondSurname;
+
+	@Column(name = "BIRTH_DATE")
+	@NotNull
+	private String birth;
+
+	@Column(name = "NIF")
+	private String nif;
+
+	@Column(name = "PHONE")
+	private String phone;
+
+	@Column(name = "AUXILIAR_PHONE")
+	private String phoneAux;
+
+	@Column(name = "PHOTO")
+	private String photoPath;
+
+	@Column(name = "USERNAME")
+	@NotNull
+	private String username;
+
+	@Column(name = "ROLE") //ENUM
+	private String role;
+
+	@Column(name = "EMAIL")
+	@NotNull
 	private String emailId;
-	private String password;
-	private boolean enabled;
+
+	@Column(name = "LANGUAGE", columnDefinition = "varchar default 'CATALAN''") //ENUM
+	private String language;
+
+	@Column(name = "NOTIFICATIONS", columnDefinition = "boolean default true")
+	private String notificiationsEnabled;
+
+	@OneToMany(targetEntity=Procedure.class, mappedBy="user")
+	//@JoinColumn(name = "PROCEDURE_ID") <---- HO HEM DE POSAR?
+	private List<Procedure> procedures;
+
+	//@OneToMany(mappedBy = "cart", cascade = CascadeType.ALL,fetch=FetchType.EAGER)
+	//private List<CartItem> cartItem;
 
 	@OneToOne(mappedBy = "users")
 	private Customer customer;
 
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
+	public User() {
 	}
 
 	public String getUserId() {
@@ -42,20 +83,84 @@ public class User implements Serializable {
 		this.userId = userId;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getName() {
+		return name;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public boolean isEnabled() {
-		return enabled;
+	public String getSurname() {
+		return surname;
 	}
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getSecondSurname() {
+		return secondSurname;
+	}
+
+	public void setSecondSurname(String secondSurname) {
+		this.secondSurname = secondSurname;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPhoneAux() {
+		return phoneAux;
+	}
+
+	public void setPhoneAux(String phoneAux) {
+		this.phoneAux = phoneAux;
+	}
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
 	}
 
 	public String getEmailId() {
@@ -66,4 +171,27 @@ public class User implements Serializable {
 		this.emailId = emailId;
 	}
 
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getNotificiationsEnabled() {
+		return notificiationsEnabled;
+	}
+
+	public void setNotificiationsEnabled(String notificiationsEnabled) {
+		this.notificiationsEnabled = notificiationsEnabled;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
 }
