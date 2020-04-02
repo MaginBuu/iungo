9ec558ccf120bf84768dc0338b089c4cb8c16ac0
Magí Tell Bonet <mtellb@gmail.com>
2020-04-02 15:58:54 +0200

    department added to roleTeacher

--- src/main/java/com/model/RoleTeacher.java
+++ src/main/java/com/model/RoleTeacher.java
@@ -1,6 +1,7 @@
 package com.model;
 
 
+import com.model.enums.Department;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -15,14 +16,16 @@ public class RoleTeacher extends RoleClass {
     @ManyToMany(mappedBy="teachers" , fetch=FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Subject> subjects = new LinkedList<>();
 
+    @Column(name = "DEPARTMENT")
+    public Department department;
+
     public RoleTeacher() {}
 
     public void addSubject(Subject subject){
         this.subjects.add(subject);
     }
 
+    public Department getDepartment() { return department; }
 
-
-
-
+    public void setDepartment(Department department) { this.department = department; }
 }
